be4a7f7ea39cc2ec49e27d428a90ebbd531efd98
yonB280405 <giabao2005bbb@gmail.com>
2024-11-22 00:13:38 +0700

    Add: Send mail (Mailer)

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,12 @@ import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './middleware/passport/jwt-auth.guard';
 import { UsersModule } from './users/module/users.module';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
     UsersModule,
+    AuthModule,
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
@@ -19,7 +21,33 @@ import { UsersModule } from './users/module/users.module';
       }),
       inject: [ConfigService]
     }),
-    AuthModule
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        transport: {
+          host: 'smtp.gmail.com',
+          port: 465, // 465 SSL(Bảo mật)/secure: true hoặc 587 TLS(Không bảo mật)/secure: false
+          secure: true,
+          // ignoreTLS: true,
+          auth: {
+            user: configService.get<string>('MAIL_USER'),
+            pass: configService.get<string>('MAIL_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: '"Giáo Xứ Tân Trang" <no-reply@giaoxutantrang@gmail.com>',
+        },
+        // preview: true,
+        // template: {
+        //   dir: process.cwd() + '/src/mail/templates/',
+        //   adapter: new HandlebarsAdapter(),
+        //   options: {
+        //     strict: true,
+        //   },
+        // },
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,11 +4,15 @@ import { LocalAuthGuard } from '@/middleware/passport/local.auth.guard';
 import { JwtAuthGuard } from '@/middleware/passport/jwt-auth.guard';
 import { Public } from '@/decorator/customize';
 import { CreateAuthDto } from './dto/create-auth.dto';
+import { MailerService } from '@nestjs-modules/mailer';
 
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) { }
+  constructor(
+    private readonly authService: AuthService,
+    private readonly mailerService: MailerService
+  ) { }
 
   @Post("login")
   @Public()
@@ -21,5 +25,18 @@ export class AuthController {
   @Public()
   register(@Body() registerDto: CreateAuthDto) {
     return this.authService.handleRegister(registerDto);
+  }
+
+  @Get('mail')
+  @Public()
+  sendEmail() {
+    this.mailerService
+      .sendMail({
+        to: 'baonguyxndev@gmail.com', // list of receivers
+        subject: 'Testing Nest MailerModule ✔', // Subject line
+        text: 'welcome', // plaintext body
+        html: '<b>test send email</b>'
+      })
+    return "ok";
   }
 }
